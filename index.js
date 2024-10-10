9c591c6110283e8d0aa29a6691b7aeec6d666720
sktanui <shadrackkemei2019@gmail.com>
2024-10-10 11:30:42 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',  
+        headers: {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json'  
+        },
+        body: JSON.stringify({
+            name: name,   
+            email: email  
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+         
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = `New ID: ${data.id}`;
+        body.appendChild(p);
+    })
+     
+    .catch(error => {
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = `Error: ${error.message}`;
+        body.appendChild(p);
+    });
+}
+
+
